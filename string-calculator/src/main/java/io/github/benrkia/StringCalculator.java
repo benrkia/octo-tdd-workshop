da4e69620f838755067e6bd1dcfb2bcd014c27d5
ilyasse benrkia <ilyasse.benrkia@octo.ma>
2022-05-10 21:12:14 +0100

    Solution

--- string-calculator/src/main/java/io/github/benrkia/StringCalculator.java
+++ string-calculator/src/main/java/io/github/benrkia/StringCalculator.java
@@ -1,6 +1,50 @@
 package io.github.benrkia;
 
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class StringCalculator {
 
+  public static final int MAX_NUMBER_THRESHOLD = 1000;
+
+  public int sum(final String input) {
+    if (input.length() == 0)
+      return 0;
+
+    var parsedNumbers = parseNumbersBelowThreshold(input);
+    ensureNonNegativeNumbers(parsedNumbers);
+
+    int sum = 0;
+    for (int number : parsedNumbers) {
+      sum += number;
+    }
+    return sum;
+  }
+
+  private void ensureNonNegativeNumbers(final List<Integer> parsedNumbers) {
+    List<Integer> negativeNumbers = parsedNumbers.stream()
+        .filter(number -> number < 0)
+        .collect(Collectors.toList());
+
+    if (!negativeNumbers.isEmpty()) {
+      throw new NumberFormatException("Negative numbers are not allowed " + negativeNumbers);
+    }
+  }
+
+  private List<Integer> parseNumbersBelowThreshold(final String input) {
+    return Stream.of(input.split("[,\n]"))
+        .map(this::parse)
+        .filter(number -> number < MAX_NUMBER_THRESHOLD)
+        .collect(Collectors.toList());
+  }
+
+  private int parse(final String number) {
+    try {
+      return Integer.parseInt(number);
+    } catch (NumberFormatException e) {
+      throw new NumberFormatException("Invalid number");
+    }
+  }
 }
--- string-calculator/src/test/java/io/github/benrkia/StringCalculatorTest.java
+++ string-calculator/src/test/java/io/github/benrkia/StringCalculatorTest.java
@@ -1,7 +1,111 @@
 package io.github.benrkia;
 
-public class StringCalculatorTest
-{
-    private final StringCalculator calculator = new StringCalculator();
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class StringCalculatorTest {
+  // AAA => Arrange, Act, Assert
+  // SUT => system under test
+
+  @Test
+  void should_return_zero_when_provided_empty_string() {
+    // Arrange
+    var sut = new StringCalculator();
+
+    // Act
+    var actual = sut.sum("");
+
+    // Assert
+    assertEquals(0, actual);
+  }
+
+  @ParameterizedTest
+  @MethodSource
+  void should_return_a_number_value(final String input, final int expected) {
+    // Arrange
+    var sut = new StringCalculator();
+
+    // Act
+    var actual = sut.sum(input);
+
+    // Assert
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void should_sum_comma_delimited_numbers() {
+    // Arrange
+    var sut = new StringCalculator();
+
+    // Act
+    var actual = sut.sum("22,7");
+
+    // Assert
+    assertEquals(29, actual);
+  }
+
+  @Test
+  void should_throw_when_provided_invalid_number() {
+    // Arrange
+    var sut = new StringCalculator();
+
+    // Assert
+    String message = assertThrows(NumberFormatException.class, () -> {
+      // Act
+      sut.sum("22,ab");
+    }).getMessage();
+
+    // Assert
+    assertEquals("Invalid number", message);
+  }
+
+  @Test
+  void should_sum_newline_delimited_numbers() {
+    // Arrange
+    var sut = new StringCalculator();
+
+    // Act
+    var actual = sut.sum("22\n7");
+
+    // Assert
+    assertEquals(29, actual);
+  }
+
+  @Test
+  void should_throw_when_provided_negative_number() {
+    // Arrange
+    var sut = new StringCalculator();
+
+    // Assert
+    String message = assertThrows(NumberFormatException.class, () -> {
+      // Act
+      sut.sum("23,25\n-47\n-199");
+    }).getMessage();
+
+    assertEquals("Negative numbers are not allowed [-47, -199]", message);
+  }
+
+  @Test
+  void should_ignore_numbers_greater_than_thousand() {
+    var sut = new StringCalculator();
+
+    var actual = sut.sum("50,1000\n500,38983");
+
+    assertEquals(550, actual);
+  }
+
+  private static Stream<Arguments> should_return_a_number_value() {
+    return Stream.of(
+        Arguments.of("5", 5),
+        Arguments.of("28", 28),
+        Arguments.of("500", 500)
+    );
+  }
 }
